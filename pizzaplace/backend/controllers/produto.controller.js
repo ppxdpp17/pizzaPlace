a55b122bc70d220ec38155b3668e0d3544d31aec
Pedro <pedrosapageduarte@hotmail.com>
2025-07-02 15:13:34 +0100

    Produtos quase prontos

--- pizzaplace/backend/controllers/produto.controller.js
+++ pizzaplace/backend/controllers/produto.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Produto  from "../models/produto.model.js";
 
 export const getAllProdutos = async (req, res) => {
@@ -10,3 +11,29 @@ export const getAllProdutos = async (req, res) => {
         res.status(500).json({msg: "Erro no servidor", error: error.message});
     }
 };
+
+export const getProdutosDisponiveis = async (req, res) => {
+    try {
+        let produtosDisponiveis = await redis.get("produtos_disponiveis");
+        if(produtosDisponiveis)
+        {
+            return res.json(JSON.parse(produtosDisponiveis));
+        }
+
+        //Se não estiver no redis, vamos buscar ao mongodb
+        produtosDisponiveis = await Produto.find({disponivel: true}).lean();    //.lean() vai retornar um objeto javascript
+        if(!produtosDisponiveis)                                                //em vez de um documento mongodb, o que é 
+                                                                                //melhor para performance
+        {
+            return res.status(404).json({msg: "Nenhum produto disponível."});
+        }
+
+        //Guardar no redis para futuro acesso rápido
+        await redis.set("produtos_disponiveis", JSON.stringify(produtosDisponiveis));
+
+        res.json(produtosDisponiveis);
+    } catch (error) {
+        console.log("Erro no controller de produtos", error.message);
+        res.status(500).json({msg: "Erro no servidor", error: error.message});
+    }
+}
--- pizzaplace/backend/routes/produtos.route.js
+++ pizzaplace/backend/routes/produtos.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllProdutos } from "../controllers/produto.controller.js";
+import { getAllProdutos, getProdutosDisponiveis } from "../controllers/produto.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProdutos);
+router.get("/disponiveis", getProdutosDisponiveis);
+router.post("/", protectRoute, adminRoute, criarProduto);
 
 export default router
