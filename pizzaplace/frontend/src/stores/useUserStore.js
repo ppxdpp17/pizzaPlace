7f0a404382cefc47d2c8af2402741fa1cc4e52e5
Pedro <pedrosapageduarte@hotmail.com>
2025-07-22 20:11:42 +0100

    Bug fixes

--- pizzaplace/frontend/src/stores/useUserStore.js
+++ pizzaplace/frontend/src/stores/useUserStore.js
@@ -1,7 +1,6 @@
 import  { create } from "zustand";
 import axios from "../lib/axios";
 import { toast } from "react-hot-toast";
-import { response } from "express";
 
 export const useUserStore = create((set, get) => ({
     user: null,
