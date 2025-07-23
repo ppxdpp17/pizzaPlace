7324858f9c2425fc172b6cd81537fb03ba0246eb
Pedro <pedrosapageduarte@hotmail.com>
2025-07-23 12:29:55 +0100

    useAuthStore inserido para novo SignUp

--- pizzaplace/frontend/src/pages/SignUpPage.jsx
+++ pizzaplace/frontend/src/pages/SignUpPage.jsx
@@ -4,7 +4,7 @@ import { Loader, Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import MedidorForcaPassword from "../components/MedidorForcaPassword";
-//import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from "../stores/useAuthStore.js";
 
 const SignUpPage = () => {
   const [name, setName] = useState("");
---
+++ pizzaplace/frontend/src/stores/useAuthStore.js
@@ -0,0 +1,98 @@
+import { create } from "zustand";
+import axios from "axios";
+
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+
+axios.defaults.withCredentials = true;
+
+export const useAuthStore = create((set) => ({
+	user: null,
+	isAuthenticated: false,
+	error: null,
+	isLoading: false,
+	isCheckingAuth: true,
+	message: null,
+
+	signup: async (email, password, name) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URL}/signup`, { email, password, name });
+			set({ user: response.data.user, isAuthenticated: true, isLoading: false });
+		} catch (error) {
+			set({ error: error.response.data.message || "Erro ao criar conta", isLoading: false });
+			throw error;
+		}
+	},
+	login: async (email, password) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URL}/login`, { email, password });
+			set({
+				isAuthenticated: true,
+				user: response.data.user,
+				error: null,
+				isLoading: false,
+			});
+		} catch (error) {
+			set({ error: error.response?.data?.message || "Erro ao entrar", isLoading: false });
+			throw error;
+		}
+	},
+
+	logout: async () => {
+		set({ isLoading: true, error: null });
+		try {
+			await axios.post(`${API_URL}/logout`);
+			set({ user: null, isAuthenticated: false, error: null, isLoading: false });
+		} catch (error) {
+			set({ error: "Erro ao sair", isLoading: false });
+			throw error;
+		}
+	},
+	verifyEmail: async (code) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URL}/verify-email`, { code });
+			set({ user: response.data.user, isAuthenticated: true, isLoading: false });
+			return response.data;
+		} catch (error) {
+			set({ error: error.response.data.message || "Erro ao verificar email", isLoading: false });
+			throw error;
+		}
+	},
+	checkAuth: async () => {
+		set({ isCheckingAuth: true, error: null });
+		try {
+			const response = await axios.get(`${API_URL}/check-auth`);
+			set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: false });
+		} catch (error) {
+			set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+		}
+	},
+	forgotPassword: async (email) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URL}/forgot-password`, { email });
+			set({ message: response.data.message, isLoading: false });
+		} catch (error) {
+			set({
+				isLoading: false,
+				error: error.response.data.message || "Erro no envio do email de redefinição de password",
+			});
+			throw error;
+		}
+	},
+	resetPassword: async (token, password) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
+			set({ message: response.data.message, isLoading: false });
+		} catch (error) {
+			set({
+				isLoading: false,
+				error: error.response.data.message || "Erro ao redifinir a passwords",
+			});
+			throw error;
+		}
+	},
+}));
