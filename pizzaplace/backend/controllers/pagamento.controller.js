05397e0aa1899952382a4c43c7197f67c18937ad
Pedro <pedrosapageduarte@hotmail.com>
2025-08-12 12:40:43 +0100

    Novos packages

--- pizzaplace/backend/controllers/pagamento.controller.js
+++ pizzaplace/backend/controllers/pagamento.controller.js
@@ -218,7 +218,7 @@ export const cashPayment = async (req, res) => {
         tipoEntrega,
         metodoPagamento: "dinheiro",
         shippingAddress,
-        estado: "a cozinhar"
+        estado: "A cozinhar"
     });
 
 
