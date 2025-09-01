2ce9ff87337dcab4adfb53f0f1364115fd38f41b
Pedro <pedrosapageduarte@hotmail.com>
2025-09-01 14:33:09 +0100

    Adição de model de ingredientes

---
+++ pizzaplace/backend/models/ingrediente.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const ingredienteSchema = new mongoose.Schema({
+  nome: { type: String, required: true, trim: true },
+  icone: { type: String }
+}, { timestamps: true });
+
+const Ingrediente = mongoose.model("Ingrediente", ingredienteSchema);
+export default Ingrediente;
