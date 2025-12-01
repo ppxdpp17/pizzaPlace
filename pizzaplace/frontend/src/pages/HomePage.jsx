78d313ac5abda33f6005167318fbd4eca0d5dda8
unknown <pedrosapageduarte@hotmail.com>
2025-12-01 14:16:36 +0000

    Inicio das alterações de UI

--- pizzaplace/frontend/src/pages/HomePage.jsx
+++ pizzaplace/frontend/src/pages/HomePage.jsx
@@ -11,32 +11,32 @@ const categorias = [
 
 const HomePage = () => {
 
-const { fetchProdutosRecomendados, products, isLoading } = useProductStore();
+  const { fetchProdutosRecomendados, products, isLoading } = useProductStore();
 
-useEffect(() => {
-  fetchProdutosRecomendados({ size: 6 });
-}, [fetchProdutosRecomendados]);
+  useEffect(() => {
+    fetchProdutosRecomendados({ size: 6 });
+  }, [fetchProdutosRecomendados]);
 
   return (
-  <div className="relative min-h-screen text-white overflow-hidden">
-    <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-      <h1 className="text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4">
-        Explore as Nossas Categorias
-      </h1>
-      <p className="text-center text-xl text-gray-300 mb-12">
-        Descubra as últimas trends da moda.
-      </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {categorias.map((categoria => (
-          <CategoryItem
-            categoria={categoria}
-            key={categoria.nome}
-          />
-        )))}
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <h1 className="text-center text-5xl sm:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-600 mb-4">
+          Explore as Nossas Categorias
+        </h1>
+        <p className="text-center text-xl text-gray-300 mb-12">
+          Descubra as últimas trends da moda.
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {categorias.map((categoria => (
+            <CategoryItem
+              categoria={categoria}
+              key={categoria.nome}
+            />
+          )))}
+        </div>
+        {!isLoading && products.length > 0 && <ProdutosRecomendados produtosRecomendados={products} />}
       </div>
-      {!isLoading && products.length > 0 && <ProdutosRecomendados produtosRecomendados={products} />}
     </div>
-  </div>
   )
 };
 
--- pizzaplace/frontend/src/pages/LoginPage.jsx
+++ pizzaplace/frontend/src/pages/LoginPage.jsx
@@ -7,22 +7,22 @@ import { useAuthStore } from "../stores/useAuthStore.js";
 import { useUserStore } from "../stores/useUserStore.js";
 
 const LoginPage = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const { login, isLoading, error } = useAuthStore();
-  const { verificarAutenticacao } = useUserStore(); 
+  const { login, isLoading, error } = useAuthStore();
+  const { verificarAutenticacao } = useUserStore();
   const navigate = useNavigate();
 
 
-	const handleLogin = async (e) => {
-		e.preventDefault();
-		await login(email, password);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    await login(email, password);
     await verificarAutenticacao();
     navigate("/");
-	};
+  };
 
-	return (
+  return (
     <div className="min-h-screen flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
@@ -31,7 +31,7 @@ const LoginPage = () => {
         className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2 xl shadow-xl overflow-hidden'
       >
         <div className='p-8'>
-          <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
+          <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-red-500 to-orange-500 text-transparent bg-clip-text'>
             Bem-Vindo
           </h2>
 
@@ -53,7 +53,7 @@ const LoginPage = () => {
             />
 
             <div className='flex justify-center mb-6'>
-              <Link to='/esqueceu-password' className='text-sm text-green-400 hover:underline'>
+              <Link to='/esqueceu-password' className='text-sm text-orange-400 hover:underline'>
                 Esqueceu-se da Password?
               </Link>
             </div>
@@ -62,7 +62,7 @@ const LoginPage = () => {
             <motion.button
               whileHover={{ scale: 1.02 }}
               whileTap={{ scale: 0.98 }}
-              className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
+              className='w-full py-3 px-4 bg-gradient-to-r from-red-500 to-orange-600 text-white font-bold rounded-lg shadow-lg hover:from-red-600 hover:to-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
               type='submit'
               disabled={isLoading}
             >
@@ -73,13 +73,13 @@ const LoginPage = () => {
         <div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
           <p className='text-sm text-gray-400'>
             Não tem uma conta?{" "}
-            <Link to='/signup' className='text-green-400 hover:underline'>
+            <Link to='/signup' className='text-orange-400 hover:underline'>
               Criar conta
             </Link>
           </p>
         </div>
       </motion.div>
     </div>
-	);
+  );
 };
 export default LoginPage;
