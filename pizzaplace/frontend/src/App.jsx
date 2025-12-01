9ef94c8dc5982e0879d506ba098c0fb5dccfd6ba
unknown <pedrosapageduarte@hotmail.com>
2025-12-01 14:23:07 +0000

    + Alterações de UI

--- pizzaplace/frontend/src/App.jsx
+++ pizzaplace/frontend/src/App.jsx
@@ -35,46 +35,46 @@ function App() {
   }, [verificarAutenticacao]);
 
   useEffect(() => {
-    if(!user) return;
-    
+    if (!user) return;
+
     getItensCarrinho()
   }, [getItensCarrinho, user])
 
-  if(checkingAuth) return <LoadingSpinner/>;
+  if (checkingAuth) return <LoadingSpinner />;
 
-  return(
+  return (
     <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
       {/**Gradiente do Background*/}
       <div className='absolute inset-0 overflow-hidden'>
         <div className='absolute inset-0'>
           <div
-            className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,_rgba(0,0,0,0.1)_100%)]"
+            className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(220,38,38,0.3)_0%,rgba(153,27,27,0.2)_45%,_rgba(0,0,0,0.1)_100%)]"
           />
         </div>
       </div>
 
       <div className='relative z-50 pt-20'>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/signup" element={ !user ? <SignUpPage/> : <Navigate to="/"/>}/>
-          <Route path="/login" element={ !user ? <LoginPage/> : <Navigate to="/"/>}/>
-          <Route path="/dashboard-secreta" element={ user?.cargo === "admin" ? <AdminPage/> : <Navigate to="/login"/>}/>
-          <Route path="/categoria/:categoria" element={ <CategoriaPage/> }/>
-          <Route path="/carrinho" element={ user ?  <CarrinhoPage/> : <Navigate to="/login" /> }/>
-          <Route path="/purchase-success" element={ user ?  <PaginaSucessoCompra/> : <Navigate to="/login" /> }/>
-          <Route path="/purchase-cancel" element={ user ?  <PaginaCancelarCompra/> : <Navigate to="/login" /> }/>
-          <Route path="/verificar-email" element={<PaginaVerificarEmail/>}/>
-          <Route path="/meus-pedidos" element={ user ? <MeusPedidos/> : <Navigate to="/login" /> }/>
-          <Route path="/esqueceu-password" element={<PaginaEsqueceuPassword/>}/>
-          <Route path="/reset-password/:token" element={<PaginaResetPassword/>}/>
-          <Route path="/admin/produtos/:id/editar" element={ user?.cargo === "admin" ? <EditProductPage/> : <Navigate to="/login" /> }/>
-          <Route path="/pizza/customizar" element={ user ? <CustomizarPizza/> : <Navigate to="/login" /> } />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path="/dashboard-secreta" element={user?.cargo === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
+          <Route path="/categoria/:categoria" element={<CategoriaPage />} />
+          <Route path="/carrinho" element={user ? <CarrinhoPage /> : <Navigate to="/login" />} />
+          <Route path="/purchase-success" element={user ? <PaginaSucessoCompra /> : <Navigate to="/login" />} />
+          <Route path="/purchase-cancel" element={user ? <PaginaCancelarCompra /> : <Navigate to="/login" />} />
+          <Route path="/verificar-email" element={<PaginaVerificarEmail />} />
+          <Route path="/meus-pedidos" element={user ? <MeusPedidos /> : <Navigate to="/login" />} />
+          <Route path="/esqueceu-password" element={<PaginaEsqueceuPassword />} />
+          <Route path="/reset-password/:token" element={<PaginaResetPassword />} />
+          <Route path="/admin/produtos/:id/editar" element={user?.cargo === "admin" ? <EditProductPage /> : <Navigate to="/login" />} />
+          <Route path="/pizza/customizar" element={user ? <CustomizarPizza /> : <Navigate to="/login" />} />
         </Routes>
       </div>
       <Toaster />
     </div>
-  ); 
+  );
 }
 
 export default App
