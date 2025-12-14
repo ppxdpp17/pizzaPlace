c2f38bc3b51aeb037d4b36129ead1ed009cb5ed5
unknown <pedrosapageduarte@hotmail.com>
2025-12-14 12:46:42 +0000

    Mudanças UI

--- pizzaplace/frontend/src/components/CategoryItem.jsx
+++ pizzaplace/frontend/src/components/CategoryItem.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const CategoryItem = ({ categoria }) => {
     return (
-        <div className="relative overflow-hidden h-96 w-full rounded-lg group bg-orange-50">
+        <div className="relative overflow-hidden h-96 w-full rounded-lg group bg-amber-100">
             <Link to={"/categoria" + categoria.href} >
                 <div className="w-full h-full cursor-pointer">
                     <img src={categoria.imageUrl}
@@ -11,8 +11,8 @@ const CategoryItem = ({ categoria }) => {
                     ease-out group-hover:scale-110"
                         loading="lazy" />
                     <div className="absolute bottom-0 left-0 right-0 p-4 z-20">
-                        <h3 className="text-gray-900 text-2xl font-bold mb-2">{categoria.nome}</h3>
-                        <p className="text-gray-700 text-sm">Explorar {categoria.nome}</p>
+                        <h3 className="text-red-700 text-2xl font-bold mb-2">{categoria.nome}</h3>
+                        <p className="text-orange-900 text-sm">Explorar {categoria.nome}</p>
                     </div>
                 </div>
             </Link>
